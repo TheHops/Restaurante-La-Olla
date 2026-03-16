8f0f81d8b562a79ea33750ef520b53e1516dbdc4
TheHops <jasson2852@gmail.com>
2026-03-15 23:10:33 -0600

    fix: templates

--- RestauranteLaOlla/settings.py
+++ RestauranteLaOlla/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'RestauranteLaOlla.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['RestauranteLaOlla/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
