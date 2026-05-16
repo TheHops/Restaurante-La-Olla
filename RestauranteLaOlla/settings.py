23090fa967769259919ee68ef1678eff2d475a50
TheHops <jasson2852@gmail.com>
2026-05-15 20:07:49 -0600

    feat: Archivos estáticos

--- RestauranteLaOlla/settings.py
+++ RestauranteLaOlla/settings.py
@@ -164,11 +164,11 @@ MEDIA_URL = '/media/'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # CORRECTO
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # CORRECTO
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # CORRECTO
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # CORRECTO
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
