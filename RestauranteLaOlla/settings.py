68fa7a46868895167d62d6078a0c2a6d0b92b200
TheHops <jasson2852@gmail.com>
2026-06-18 22:12:46 -0600

    feat: CompressedStaticFileStorage

--- RestauranteLaOlla/settings.py
+++ RestauranteLaOlla/settings.py
@@ -183,6 +183,7 @@ if not DEBUG:
             "BACKEND": "django.core.files.storage.FileSystemStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            # "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"
         },
     }
